9ff2e19efdc6262c6850daf4923fa197f1749d8f
valeria <urgbal@gmail.com>
2019-11-16 20:57:15 +0300

    fixed music

--- Assets/Scripts/MyAudioManager.cs
+++ Assets/Scripts/MyAudioManager.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class MyAudioManager : MonoBehaviour
 {
     public AudioClip[] music_list;
+    private int counter = 0;
 
     public void PlayMusicOnLevel(int level)
     {
-        if (level < music_list.Length*2)
-             AudioManager.Instance.PlayMusic(music_list[level/2]);
-            
+        if (level < music_list.Length*2 && counter == 0)
+        {
+            AudioManager.Instance.PlayMusic(music_list[level/2]);
+        }
+        if (level < music_list.Length * 2 && counter < 2)
+        {
+            counter++;
+        }
+        if (level < music_list.Length * 2 && counter >= 2)
+        {
+            counter = 0;
+        }
+
+
+
+
     }
 }
