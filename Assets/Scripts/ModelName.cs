30228bc51f6b609317201c6a653ac71ebd941e76
valeria <urgbal@gmail.com>
2019-11-16 13:36:32 +0300

    model names
    added model names

---
+++ Assets/Scripts/ModelName.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class ModelName : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public GameObject player;
+    public float time = 1.0f;
+    private Camera cam;
+    private Animator anim;
+    void Start()
+    {
+        cam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        anim = gameObject.GetComponent<Animator>();
+        StartCoroutine(Disappear());
+    }
+
+    public void ChangeName(string name)
+    {
+        text.text = name;
+    }
+    void Update()
+    {
+        transform.position = cam.WorldToScreenPoint(player.GetComponent<Transform>().position + new Vector3(0, 1f, 0));
+    }
+    private IEnumerator Disappear()
+    {
+        yield return new WaitForSeconds(time);
+        anim.SetTrigger("disappearTrigger");
+        Destroy(gameObject, 2); // this is for 2 second delay
+    }
+
+}
